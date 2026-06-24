5bcfd56946a617f4ae0def4dd4e9455f09d9e629
Fayaz Faizal <fayazblogger@gmail.com>
2026-06-24 17:29:12 +0530

    crud completed

--- app/routers/todos.py
+++ app/routers/todos.py
@@ -1,10 +1,10 @@
 from pydantic import BaseModel
-from fastapi import Depends
-from fastapi import APIRouter
+from fastapi import Depends, APIRouter, Response
+from fastapi.exceptions import HTTPException
 from sqlalchemy import select
 from sqlalchemy.orm import Session
 from app.database import get_db
-from app.schemas.todo import TodoCreate, TodoResponse
+from app.schemas.todo import TodoCreate, TodoResponse, TodoUpdate
 from app.models.todo import Todo
 
 router = APIRouter(
@@ -30,3 +30,30 @@ def list_todos(db:Session=Depends(get_db)):
     results = db.execute(statement=statement)
     todos = results.scalars().first()
     return todos
+
+@router.patch("/{todo_id}", response_model=TodoResponse)
+def update_todo(todo_id: int, todo_in:TodoUpdate,  db:Session = Depends(get_db)):
+    statement = select(Todo).where(Todo.id == todo_id)
+    result = db.execute(statement)
+    todo = result.scalar()
+    if not todo:
+        raise HTTPException(status_code=404, detail="Todo Not found")
+    update_data = todo_in.model_dump(exclude_unset=True)
+    for key, value in update_data.items():
+        setattr(todo, key, value)
+    db.commit()
+    db.refresh(todo)
+    return todo
+
+@router.delete("/{todo_id}")
+def delete_todo(todo_id: int, db:Session = Depends(get_db)):
+    statement = select(Todo).where(Todo.id == todo_id)
+    result = db.execute(statement)
+    todo_obj = result.scalar()
+    if not todo_obj:    
+        raise HTTPException(status_code=404, detail="Todo Not found")
+    
+    db.delete(todo_obj)
+    db.commit()
+    return Response(status_code=204)
+    
--- app/schemas/todo.py
+++ app/schemas/todo.py
@@ -1,4 +1,6 @@
+from typing import Optional
 from pydantic import BaseModel
+
 
 class TodoCreate(BaseModel):
     title: str
@@ -8,3 +10,7 @@ class TodoResponse(BaseModel):
     id: int 
     title: str
     completed: bool = False
+    
+class TodoUpdate(BaseModel):
+    title: Optional[str] = None
+    completed: Optional[bool] = None
